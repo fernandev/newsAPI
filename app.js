8c550c31fc8552300a5f2853205d4b196d7ce570
Marcos Fernandes <rnvrcos@gmail.com>
2019-02-13 14:43:48 -0200

    Allowing the usage of express-validator on the app.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const expressValidator = require('express-validator');
+
 const newsRoutes = require('./API/routes/news');
 
 const app = express();
+app.use(expressValidator());
 
 app.use('/news', newsRoutes);
 
